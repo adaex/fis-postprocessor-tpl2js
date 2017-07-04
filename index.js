b097d8113b4d6ae4c3b81a0ff7c72986451fc77c
杨涛 <spryti@qq.com>
2017-07-04 14:14:13 +0800

    优化细节

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
 module.exports = function (content, file, conf) {
     content = content.replace(/\s*\n\s*/g, '');
-    content = content.replace(/<script[^>]*?>.*?<\/script>/ig, '');
-    content = content.replace(/<style[^>]*?>.*?<\/style>/ig, '');
-    content = content.replace(/<!--[/!]*?[^<>]*?>/ig, '');
+    content = content.replace(/<script.*?<\/script>/ig, '');
+    content = content.replace(/<style.*?<\/style>/ig, '');
+    content = content.replace(/<!--.*?-->/g, '');
     content = content.replace(/'/g, "\\'");
+
     if (conf.tpl) {
-        content = conf.tpl.split('${content}').join(content)
-        content = content.split('${id}').join(file.moduleId)
+        content = conf.tpl.split('${content}').join(content);
+        content = content.split('${id}').join(file.moduleId);
     } else {
-        content = "define('" + file.moduleId + "',[],function(){return '" + content + "'});";
+        content = "define('" + file.moduleId + "',function(){return '" + content + "'});";
     }
     return content;
 };
